0d4a6c3a6f2e7c1bc2e0330a999e0889cc6d1883
thinhschool1357 <104267661+trthinh2003@users.noreply.github.com>
2025-04-09 10:44:56 +0700

    nothing

--- resources/views/components/_navbar.blade.php
+++ resources/views/components/_navbar.blade.php
@@ -159,7 +159,11 @@
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                <img class="img-profile rounded-circle" src="{{ asset('uploads/' . $employee[0]->avatar ?? '') }}">
+                @if (!empty($employee) && isset($employee[0]->avatar))
+                    <img class="img-profile rounded-circle" src="{{ asset('uploads/' . $employee[0]->avatar) }}">
+                @else
+                    <img class="img-profile rounded-circle" src="{{ asset('uploads/default-avatar.jpg') }}">
+                @endif
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
